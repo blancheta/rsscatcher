1c19bbfea073b5e622ac76dd987a21f4fdb52033
ablanchet <alexandreblanchet44@gmail.com>
2018-07-12 10:31:03 +0200

    feature-6 / change method views to class views

--- src/dashboard/views/__init__.py
+++ src/dashboard/views/__init__.py
@@ -2,6 +2,6 @@ from .discover import DiscoverView
 from .dashboard import dashboard
 from .sidebar import sidebar
 from .views import (
-    FeedPosts, feed_post, post_change_state,
-    FilterPosts, comments_view
+    FeedPosts, FeedPost, PostChangeState,
+    FilterPosts, CommentView, FeedPostNewComment
 )
